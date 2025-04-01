45b94705db211e8ba7eb8227228a9db3e41b3659
SolStar <44028047+ewokswagger@users.noreply.github.com>
2025-03-31 21:04:29 -0400

    Give Paramedics Emergency JoL (#3219)
    * changes
    
    * antag advantage
    
    * Move body bag to locker for more space
    
    * loadout changes
    
    * space
    
    * space space
    
    * forgot one
    
    * commit comment
    
    * Consistency
    
    * Seperated JoL into emergency and advanced
    
    * broken
    
    * running
    
    * Working
    
    * comment
    
    * fish
    
    * meow
    
    * move

--- Content.Shared/Prying/Systems/PryingSystem.cs
+++ Content.Shared/Prying/Systems/PryingSystem.cs
@@ -72,7 +72,7 @@ public sealed class PryingSystem : EntitySystem
         if (!comp.Enabled)
             return false;
 
-        if (!CanPry(target, user, out var message, comp))
+        if (!CanPry(target, user, out var message, comp, tool: tool)) // DeltaV - Add tool
         {
             if (!string.IsNullOrWhiteSpace(message))
                 _popup.PopupClient(Loc.GetString(message), target, user);
@@ -113,7 +113,11 @@ public sealed class PryingSystem : EntitySystem
         return StartPry(target, user, null, modifier, out id);
     }
 
-    private bool CanPry(EntityUid target, EntityUid user, out string? message, PryingComponent? comp = null, PryUnpoweredComponent? unpoweredComp = null)
+    /// <summary>
+    /// DeltaV - Add tool field
+    /// </summary>
+    /// <param name="tool">DeltaV - The tool uid if one is being used.</param>
+    private bool CanPry(EntityUid target, EntityUid user, out string? message, PryingComponent? comp = null, PryUnpoweredComponent? unpoweredComp = null, EntityUid? tool = null)
     {
         BeforePryEvent canev;
 
@@ -133,6 +137,11 @@ public sealed class PryingSystem : EntitySystem
         }
 
         RaiseLocalEvent(target, ref canev);
+
+        // Begin DeltaV - Raise event on tool if one is being used.
+        if (tool is {} toolUid)
+            RaiseLocalEvent(toolUid, ref canev);
+        // End DeltaV - Raise event on tool if one is being used.
 
         message = canev.Message;
 
@@ -171,7 +180,7 @@ public sealed class PryingSystem : EntitySystem
 
         TryComp<PryingComponent>(args.Used, out var comp);
 
-        if (!CanPry(uid, args.User, out var message, comp))
+        if (!CanPry(uid, args.User, out var message, comp, tool: args.Used)) // DeltaV - Check tool for power
         {
             if (!string.IsNullOrWhiteSpace(message))
                 _popup.PopupClient(Loc.GetString(message), uid, args.User);
@@ -185,6 +194,10 @@ public sealed class PryingSystem : EntitySystem
 
         var ev = new PriedEvent(args.User);
         RaiseLocalEvent(uid, ref ev);
+        // Begin DeltaV - Emergency JoL uses power.
+        if (args.Used is {} tool)
+            RaiseLocalEvent(tool, ref ev);
+        // End DeltaV - Emergency JoL uses power.
     }
 }
 
